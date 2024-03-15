0fc4028e22c2c5a65af3b8656acb121e43a34859
tuts85 <arthur.mlazaro@gmail.com>
2024-03-15 11:09:20 -0600

    complete project with model and prediction

--- salaries.R
+++ salaries.R
@@ -57,9 +57,7 @@ adult[adult == '?'] <- NA
 library(Amelia)
 missmap(adult,y.at=c(1),y.labels = c(''),col=c('yellow','black'))
 
-blank.type.employer <- adult[is.na(adult$type_employer),]
-
-adult.cleaned <- adult[complete.cases(adult$occupation),]
+adult <- na.omit(adult)
   
 #text to factor
 adult <- adult %>% mutate_if(is.character, as.factor)
@@ -69,4 +67,56 @@ summary(adult)
 
 ### EDA
 library(ggplot2)
-ggplot(adult.cleaned,aes(age)) + geom_histogram(aes(fill=income),color='black',binwidth=1) + theme_bw()
+
+#Histogram of age colored by salaries
+ggplot(adult,aes(age)) + geom_histogram(aes(fill=income),color='black',binwidth=1) + theme_bw()
+
+#histogram of hours worked per week
+ggplot(adult,aes(hr_per_week)) + geom_histogram(aes(fill=income), binwidth=3) + theme_bw()
+
+# barplot of continent with the fill color defined by income class.
+ggplot(adult, aes(continent)) + geom_bar(aes(fill=income), color='black') + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+adult <- select(adult, -country)
+## Creating the model
+
+# Splitting into train and test using catools
+install.packages("caTools")
+library(caTools)
+
+# Set a random see so your "random" results are the same as this notebook
+set.seed(101) 
+
+# Split up the sample, basically randomly assigns a booleans to a new column "sample"
+sample <- sample.split(adult$income, SplitRatio = 0.70) # SplitRatio = percent of sample==TRUE
+
+# Training Data
+train = subset(adult, sample == TRUE)
+
+# Testing Data
+test = subset(adult, sample == FALSE)
+
+model <- glm(income ~. , family = binomial(logit), data = train)
+
+summary(model)
+
+new.model <- step(model)
+
+summary(new.model)
+
+### Predicting on test data
+fitted.values <- predict(new.model, test, type = 'response')
+fitted.results <- ifelse(fitted.values>0.5, 1, 0)
+
+test$predicted.income = predict(model, newdata=test, type="response")
+
+table(test$income, test$predicted.income > 0.5)
+
+#model accuracy
+(6284+1348)/(6284+1348+512+904)
+
+#recall
+6284/(6284+512)
+
+#precision
+6732/(6372+904)
